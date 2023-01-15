ea151ea23cbb35299162881de080d869414396ca
OrozbayuuluTilek <121665034+OrozbayuuluTilek@users.noreply.github.com>
2023-01-16 01:16:14 +0300

    commit

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Class {
+public class Class {
+    private int number;
+    private String name;
+    private int[] array;
+    public Class(int number,String name,int[] array) {
+        this.number = number;
+        this.name = name;
+        this.array = array;
+
+
+
+    }
+ public int[] method(int[] array) {
+
+     for (int i = 0; i < array.length; i++) {
+
+
+     }
+     return array;
+ }
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class aclass = new Class(13,"Programm",new int[]{22,33,44,55,66});
+
+        System.out.println(Arrays.toString(aclass.method(new int[4])));
+        System.out.println(aclass.getNumber()+","+aclass.getName());
+
     }
 }
